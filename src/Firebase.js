1b22f771e3ef7e6acdd9d94532afdae40159b9b7
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-11-01 17:15:22 +0530

    initialised vue and firebase
    Co-authored-by: Sriram Nuthi <46497115+Nuthi-Sriram@users.noreply.github.com>

---
+++ src/Firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyDzUShJlg9_89orJfB2ZfFTWky-Hk3e85g',
+	authDomain: 'vuejs-firebase-realtime-chat.firebaseapp.com',
+	databaseURL: 'https://vuejs-firebase-realtime-chat.firebaseio.com',
+	projectId: 'vuejs-firebase-realtime-chat',
+	storageBucket: 'vuejs-firebase-realtime-chat.appspot.com',
+	messagingSenderId: '79720825114',
+	appId: '1:79720825114:web:4b9a282e7c8fab47b32ca3',
+	measurementId: 'G-LKHL4T19B3'
+};
