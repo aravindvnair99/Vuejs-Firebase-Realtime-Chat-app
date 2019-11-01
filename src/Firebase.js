feb92de9c07d932bb8c8f1c8351cec2c7fdc0c4b
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-11-01 17:32:02 +0530

    add routes
    Co-authored-by: Sriram Nuthi <46497115+Nuthi-Sriram@users.noreply.github.com>

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,6 +1,6 @@
 import * as firebase from 'firebase';
 
-const firebaseConfig = {
+const config = {
 	apiKey: 'AIzaSyDzUShJlg9_89orJfB2ZfFTWky-Hk3e85g',
 	authDomain: 'vuejs-firebase-realtime-chat.firebaseapp.com',
 	databaseURL: 'https://vuejs-firebase-realtime-chat.firebaseio.com',
@@ -10,3 +10,9 @@ const firebaseConfig = {
 	appId: '1:79720825114:web:4b9a282e7c8fab47b32ca3',
 	measurementId: 'G-LKHL4T19B3'
 };
+
+
+
+firebase.initializeApp(config);
+
+export default firebase
