7cf94442a01ddcf419f385e05cb330be7420ae2e
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-11-01 19:43:33 +0530

    Fix route bugs

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,39 @@
-import Vue form 'vue'
-import Router from 'vue-router'
-import Chat form './components/Chat.vue'
-import Room form './components/Room.vue'
-import AddRoom from './components/AddRoom.Vue'
-import Login from './components/Login.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Chat from './components/Chat.vue';
+import Room from './components/Room.vue';
+import AddRoom from './components/AddRoom.Vue';
+import Login from './components/Login.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/chat/:nickname/:roomid/:roomname',
-    name: 'Chat',
-    component: Chat
-  },
-  {
-    path: '/room/:nickname',
-    name: 'RoomList',
-    component: Room
-  },
-  {
-    path: '/add-room',
-    name: 'AddRoom',
-    component: AddRoom
-  },
-  {
-    path: '/',
-    name: 'Login',
-    component: Login
-  }
-]
+	{
+		path: '/chat/:nickname/:roomid/:roomname',
+		name: 'Chat',
+		component: Chat
+	},
+	{
+		path: '/room/:nickname',
+		name: 'RoomList',
+		component: Room
+	},
+	{
+		path: '/add-room',
+		name: 'AddRoom',
+		component: AddRoom
+	},
+	{
+		path: '/',
+		name: 'Login',
+		component: Login
+	}
+];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+	mode: 'history',
+	base: process.env.BASE_URL,
+	routes
+});
 
-export default router
+export default router;
