128349c5aaebf63a2419e8163acf69742f075b14
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-11-01 19:12:56 +0530

    Refactor

--- src/components/AddRoom.vue
+++ src/components/AddRoom.vue
@@ -18,37 +18,35 @@
 </template>
 
 <script>
-
-import firebase from '../Firebase'
-import router from '../router'
+import firebase from "../Firebase";
+import router from "../router";
 
 export default {
-  name: 'AddBoard',
-  data () {
+  name: "AddBoard",
+  data() {
     return {
-      ref: firebase.database().ref('chatrooms/'),
-      room: { roomName: '' }
-    }
+      ref: firebase.database().ref("chatrooms/"),
+      room: { roomName: "" }
+    };
   },
   methods: {
-    onSubmit (evt) {
-      evt.preventDefault()
+    onSubmit(evt) {
+      evt.preventDefault();
 
-      let newData = this.ref.push()
+      let newData = this.ref.push();
       newData.set({
         roomName: this.room.roomName
-      })
-      router.go(-1)
-      .catch((error) => {
-        alert("Error adding document: ", error)
+      });
+      router.go(-1).catch(error => {
+        alert("Error adding document: ", error);
       });
     }
   }
-}
+};
 </script>
 
 <style>
-  .jumbotron {
-    padding: 2rem;
-  }
+.jumbotron {
+  padding: 2rem;
+}
 </style>
