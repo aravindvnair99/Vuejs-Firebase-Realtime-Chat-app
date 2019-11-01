38e68ac1f356ec1fd29de6e9d522ef9467e2e8a8
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-11-01 19:51:21 +0530

    Fix bugs

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue';
-import Router from 'vue-router';
-import Chat from './components/Chat.vue';
-import Room from './components/Room.vue';
-import AddRoom from './components/AddRoom.Vue';
-import Login from './components/Login.vue';
+import VueRouter from 'vue-router';
+import Chat from '../components/Chat.vue';
+import Room from '../components/Room.vue';
+import AddRoom from '../components/AddRoom.vue';
+import Login from '../components/Login.vue';
 
 Vue.use(VueRouter);
 
